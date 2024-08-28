091b5b06588a480be169c8255d259b5d07485ee9
tuposilwe <rudigermkondya@gmail.com>
2024-08-28 09:31:30 +0300

    worker pools

--- myproject/app.go
+++ myproject/app.go
@@ -2,38 +2,41 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 
 func main(){
-c1 := make(chan string)
-c2 := make(chan string)
+  jobs := make(chan int , 100)
+  results := make(chan int,100)
 
-go func ()  {
-  for{
-    c1 <- "Every 500ms"
-    time.Sleep(time.Millisecond * 500)
+  go worker(jobs,results)
+  go worker(jobs,results)
+  go worker(jobs,results)
+  go worker(jobs,results)
+
+
+  for i := 0; i < 100; i++ {
+    jobs <- i
   }
-}()
 
-go func ()  {
-  for{
-    c2 <- "Every two second"
-    time.Sleep(time.Second * 2)
+  close(jobs)
+
+  for j:= 0; j < 100; j++ {
+    fmt.Println(<- results)
   }
-}()
-
-for{
-  select {
-  case msg1 := <- c1:
-    fmt.Println(msg1)
-  case msg2 := <- c2:
-    fmt.Println(msg2)
-  }
-}
-
 
 }
 
+func worker(jobs <- chan int, results chan <- int){
+  for n:= range jobs{
+    results <- fib(n)
+  }
+}
 
+func fib(n int) int {
+   if n <= 1 {
+    return n
+   }
+
+   return fib(n -1) + fib(n -2)
+}
