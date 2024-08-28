9a870fab9638d1f6fa55d7b63f26fa2d50d46ec8
tuposilwe <rudigermkondya@gmail.com>
2024-08-28 09:07:50 +0300

    buffered channels

--- myproject/app.go
+++ myproject/app.go
@@ -2,26 +2,15 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 
 func main(){
-  c := make(chan string)
+c := make(chan string,2)
+c <- "hello"
 
-  go count("sheep", c)
-
-  for msg := range c {
-    fmt.Println(msg)
-  }
+msg := <- c
+fmt.Println(msg)
 }
 
 
-func count(thing string, c chan string){
-   for i := 0; i <= 5; i++ {
-    c <- thing
-    time.Sleep(time.Millisecond * 500)
-   }
-
-   close(c)
-}
