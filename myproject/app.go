003ec167d9234b0127fe1a7add360ce0c4ae2092
tuposilwe <rudigermkondya@gmail.com>
2024-08-28 09:17:19 +0300

    select in channels

--- myproject/app.go
+++ myproject/app.go
@@ -2,15 +2,38 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 
 func main(){
-c := make(chan string,2)
-c <- "hello"
+c1 := make(chan string)
+c2 := make(chan string)
 
-msg := <- c
-fmt.Println(msg)
+go func ()  {
+  for{
+    c1 <- "Every 500ms"
+    time.Sleep(time.Millisecond * 500)
+  }
+}()
+
+go func ()  {
+  for{
+    c2 <- "Every two second"
+    time.Sleep(time.Second * 2)
+  }
+}()
+
+for{
+  select {
+  case msg1 := <- c1:
+    fmt.Println(msg1)
+  case msg2 := <- c2:
+    fmt.Println(msg2)
+  }
+}
+
+
 }
 
 
